8694dd1bfab3bb2e5d5b5b17715b2bd2c7b6ae1b
Andrew Minion <andrew@andrewrminion.com>
2016-02-22 21:02:30 -0500

    add quiz answers to course completion notification

--- wp-slack-wp-courseware-notifications.php
+++ wp-slack-wp-courseware-notifications.php
@@ -61,6 +61,7 @@ function wp_slack_wp_courseware_quizzes( $events ) {
         'action' => 'wpcw_user_completed_course',
         'description' => __( 'When a user completes a course', 'slack' ),
         'message' => function( $user_id, $post_id, $unit_object ) {
+            $quiz_string = NULL;
 
             // get user name
             $user = get_user_by( 'ID', $user_id );
@@ -70,12 +71,32 @@ function wp_slack_wp_courseware_quizzes( $events ) {
                 $user_name = $user->data->user_nicename;
             }
 
-            return sprintf(
-                'Course completed by *%1$s*: <%2$s|%3$s>',
+            // output user info
+            $quiz_string = sprintf(
+                'Course completed by *%1$s*: <%2$s|%3$s>' . "\n",
                 $user_name,
                 get_permalink( $post_id ),
                 wp_slack_wp_courseware_module_info( $post_id )->course_title
             );
+
+            // get quiz info
+            $quiz_answers = maybe_unserialize( get_quiz_responses( $post_id, $user_id )->quiz_data );
+            foreach ( $quiz_answers as $answer ) {
+                $quiz_string .= sprintf(
+                    '*%1$s* ' . "\n" . '>%2$s' . "\n",
+                    $answer['title'],
+                    str_replace( '&#13;&#10;', "\n" . '>', $answer['their_answer'] )
+                );
+            }
+
+            $quiz_string .= sprintf(
+                '<mailto:%1$s?subject=%2$s course on %3$s|Reply via email>',
+                $user->user_email,
+                wp_slack_wp_courseware_module_info( $post_id )->course_title,
+                get_bloginfo( 'name' )
+            );
+
+            return $quiz_string;
         }
     );
 
@@ -93,6 +114,22 @@ function wp_slack_wp_courseware_module_info( $post_id ) {
         LEFT JOIN ' . $wpdb->prefix . 'wpcw_modules m ON m.module_id = um.parent_module_id
         LEFT JOIN ' . $wpdb->prefix . 'wpcw_courses c ON c.course_id = m.parent_course_id
         WHERE um.unit_id = %d AND course_title IS NOT NULL',
+        $post_id
+    );
+
+    return $wpdb->get_row( $SQL );
+}
+
+// get quiz responses
+function get_quiz_responses( $post_id, $user_id ) {
+    global $wpdb;
+
+    $SQL = $wpdb->prepare(
+        'SELECT *
+        FROM ' . $wpdb->prefix . 'wpcw_user_progress_quizzes quiz
+        WHERE quiz.user_id = %d
+        AND unit_id = %d',
+        $user_id,
         $post_id
     );
 
